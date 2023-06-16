4790dfd99932401abf1f98c7a603e6569f8f9099
Jorge Caballero (DataDrivenMD) <116459476+DataDrivenMD@users.noreply.github.com>
2023-06-16 09:33:06 -0700

    fix: fetch limit for public paginator

--- components/timeline/TimelinePublic.vue
+++ components/timeline/TimelinePublic.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { mastodon } from 'masto'
 
-const paginator = useMastoClient().v1.timelines.listPublic({ limit: 200 })
+const paginator = useMastoClient().v1.timelines.listPublic({ limit: 30 })
 const stream = useStreaming(client => client.v1.stream.streamPublicTimeline())
 function reorderAndFilter(items: mastodon.v1.Status[]) {
   return reorderedTimeline(items, 'public', true)
